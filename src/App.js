ffa8869b50082f302db798f1ce97f5298cd81814
hyemin12 <khnm31@naver.com>
2022-04-19 15:45:59 +0900

    'projectSetting'

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import React, { useState } from "react";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import Auth from "./routes/Auth";
+import Home from "./routes/Home";
+
 function App() {
-  return <div></div>;
+  const [isLoggIn, setIsLoggIn] = useState(false);
+  return (
+    <HashRouter>
+      <Routes>
+        {isLoggIn ? (
+          <>
+            <Route exact path="/" element={<Home />}></Route>
+          </>
+        ) : (
+          <>
+            <Route exact path="/" element={<Auth />}></Route>
+          </>
+        )}
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
